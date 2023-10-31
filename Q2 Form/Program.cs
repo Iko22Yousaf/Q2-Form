fd5a826ffaafa9cfb5eb5abd0a015a10b39000bd
Ikrash <Ikrash@IKRASH>
2023-10-31 13:40:27 +0500

    Add project files.

---
+++ Q2 Form/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class RegistrationFormConsole
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Registration Form");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Enter your phone number: ");
+        string phoneNumber = Console.ReadLine();
+
+        Console.WriteLine("\nRegistration Information:");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Phone Number: {phoneNumber}");
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
